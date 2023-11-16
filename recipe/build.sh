3945b14cc1cbdf3208bbbd798460b4944abb1a37
tomsail <saillour.thomas@gmail.com>
2023-11-16 17:16:43 +0100

    tried building hdf5 with same compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,11 @@
+git clone https://github.com/HDFGroup/hdf5.git
+cd hdf5
+git checkout hdf5_1_10_9
+./configure  --prefix=$PREFIX
+make 
+make install
+
 cd $SRC_DIR/med-4.1.1 
-./configure CFLAGS=-O2 CXXFLAGS=-O2 FFLAGS=-O2 --disable-python --prefix=$PREFIX --with-hdf5=$BUILD_PREFIX
+./configure CFLAGS=-O2 CXXFLAGS=-O2 FFLAGS=-O2 --disable-python --prefix=$PREFIX --with-hdf5=$PREFIX
 make 
 make install
